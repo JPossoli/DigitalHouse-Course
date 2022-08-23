e2ed16541add81da93df27b223a9fe671a0789b1
JPossoli <65567059+JPossoli@users.noreply.github.com>
2022-08-23 00:10:57 -0300

    Create script.js

---
+++ Front_End_2/Calculator/js/script.js
@@ -0,0 +1,7 @@
+/*
+*TODO:
+*All math operations
+*Display the result in html class "display_text"
+*Diplay the operation in html class "status_text"
+*All math operations have to be in a unique JS file, for learning purpose.
+*/
